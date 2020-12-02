6de6a69d225d84bc4da5b9113ffc1c3291bf6dec
Pavel Frolov <Frolov-Works@yandex.ru>
2020-12-02 22:57:53 +0400

    init

---
+++ JS/app.js
@@ -0,0 +1,32 @@
+let click = 1;
+const TIMEOUT = 5000;
+const display = document.querySelector('.display'),
+button = document.querySelector('.button'),
+counter = document.querySelector('.counter');
+
+button.onclick = start;
+
+function start() {
+    button.onclick = () => counter.textContent = click++;
+
+    setTimeout(()=>{
+        button.onclick = null;
+        display.textContent = 'Game over';
+    }, TIMEOUT);
+
+}
+
+let test
+
+
+
+
+
+
+
+
+
+
+
+
+
