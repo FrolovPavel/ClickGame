39feebdf94da63725d996c8c5e6922943c2fc65c
Pavel Frolov <Frolov-Works@yandex.ru>
2020-12-05 17:37:33 +0400

    add button reset

--- JS/app.js
+++ JS/app.js
@@ -2,36 +2,32 @@ let click = 1;
 const TIMEOUT = 5000;
 const display = document.querySelector('.display'),
 button = document.querySelector('.button'),
-counter = document.querySelector('.counter');
+counter = document.querySelector('.counter'),
+reset = document.querySelector('.reset');
 
 button.onclick = start;
-
+reset.onclick = () => window.location.reload();
 function start() {
-    const startTime = Date.now();
-    // display.textContent = TIMEOUT;
-    button.onclick = () => counter.textContent = click++;
+        const startTime = Date.now();
+        button.onclick = () => counter.textContent = click++;
 
-    const inteval = setInterval(() => {
-        const delta = Date.now() - startTime;
-        display.textContent = formatTime(TIMEOUT - delta);
+        const inteval = setInterval(() => {
+            const delta = Date.now() - startTime;
+            display.textContent = formatTime(TIMEOUT - delta);
 
-    },100)
+        }, 100)
 
-    setTimeout(()=>{
-        button.onclick = null;
-        display.textContent = 'Game over';
-        clearInterval(inteval);
-    }, TIMEOUT);
+        setTimeout(() => {
+            button.onclick = null;
+            display.textContent = 'Game over';
 
+            clearInterval(inteval);
+        }, TIMEOUT);
+
+        function formatTime(ms) {
+            return Number.parseFloat(ms / 1000).toFixed(2);
+        }
 }
-
-function formatTime(ms) {
-    return Number.parseFloat(ms / 1000).toFixed(2);
-}
-
-
-
-
 
 
 
