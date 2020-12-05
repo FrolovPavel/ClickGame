9f1611391c9013edd73d8fb95c44cb8c6801134f
Pavel Frolov <Frolov-Works@yandex.ru>
2020-12-05 14:25:15 +0400

    add stopwatch

--- JS/app.js
+++ JS/app.js
@@ -7,18 +7,29 @@ counter = document.querySelector('.counter');
 button.onclick = start;
 
 function start() {
+    const startTime = Date.now();
+    // display.textContent = TIMEOUT;
     button.onclick = () => counter.textContent = click++;
+
+    const inteval = setInterval(() => {
+        const delta = Date.now() - startTime;
+        display.textContent = formatTime(TIMEOUT - delta);
+
+    },100)
 
     setTimeout(()=>{
         button.onclick = null;
         display.textContent = 'Game over';
+        clearInterval(inteval);
     }, TIMEOUT);
 
 }
 
-let test
-let teat
-let test2
+function formatTime(ms) {
+    return Number.parseFloat(ms / 1000).toFixed(2);
+}
+
+
 
 
 
